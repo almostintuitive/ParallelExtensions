7e51a6253c99db47c180c121f9a1f1fb2c940afd
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2016-01-15 16:47:35 +0000

    refactor: added parallelIndexOf as the base of all find related functions

--- ConcurrentMap/ConcurrentFind.swift
+++ ConcurrentMap/ConcurrentFind.swift
@@ -11,56 +11,78 @@ import Foundation
 public extension CollectionType where SubSequence : CollectionType, SubSequence.SubSequence == SubSequence, SubSequence.Generator.Element == Generator.Element, Index == Int {
   
   @warn_unused_result
-  public func parallelContains(predicate: Self.Generator.Element -> Bool) -> Bool {
-    guard !self.isEmpty else { return false }
+  public func parallelIndexOf(predicate: Self.Generator.Element -> Bool) -> Int? {
+    guard !self.isEmpty else { return nil }
     
     let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
     let group = dispatch_group_create()
     
     let batchSize: Int = Int(self.count) / 2
+    
+    var found = [Int: Int]()
+    
+    dispatch_group_async(group, queue) { () -> Void in
+      found[0] = self.batchedIndexOf(range: Range(self.startIndex...batchSize), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
+        return found[safe: 0] == nil && found[safe: 1] == nil
+      })
+    }
+    
+    dispatch_group_async(group, queue) { () -> Void in
+      found[1] = self.batchedIndexOf(range: Range(batchSize..<self.endIndex), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
+        return found[safe: 0] == nil && found[safe: 1] == nil
+      })
+    }
+    
+    
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
+    return found.values.first
+  }
+  
 
-    let found = Atomic<Bool>(false)
+  
+  
+  
+  @warn_unused_result
+  public func parallelContains(predicate: Self.Generator.Element -> Bool) -> Bool {
     
-    var found1 = false
+    if let found = self.parallelIndexOf(predicate) {
+      return true
+    } else {
+      return false
+    }
+    
+    
+//    guard !self.isEmpty else { return false }
+//    
+//    let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+//    let group = dispatch_group_create()
+//    
+//    let batchSize: Int = Int(self.count) / 2
 //
-//    found.value = self.batchedContains(range: Range(self.startIndex..<self.endIndex), batchSize: 100, predicate: predicate, checkIfContinue: { () -> Bool in
-//      return found.value == false
-//    })
-//    return found.value
-    
+//    var found = (false, false)
+//    
 //    dispatch_group_async(group, queue) { () -> Void in
-//      found.value = self.batchedContains(range: Range(self.startIndex...batchSize), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
-//        return found.value == false
+//      found.0 = self.batchedContains(range: Range(self.startIndex...batchSize), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
+//        return found.0 == false && found.1 == false
 //      })
 //    }
 //    
 //    dispatch_group_async(group, queue) { () -> Void in
-//      found.value = self.batchedContains(range: Range(batchSize..<self.endIndex), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
-//        return found.value == false
+//      found.1 = self.batchedContains(range: Range(batchSize..<self.endIndex), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
+//        return found.0 == false && found.1 == false
 //      })
 //    }
-
-    
-    dispatch_group_async(group, queue) { () -> Void in
-      found1 = self.batchedContains(range: Range(self.startIndex...batchSize), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
-        return found1 == false
-      })
-    }
-    
-    dispatch_group_async(group, queue) { () -> Void in
-      found1 = self.batchedContains(range: Range(batchSize..<self.endIndex), batchSize: max(self.count/10, 100), predicate: predicate, checkIfContinue: {
-        return found1 == false
-      })
-    }
-
-    dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
-    return found1
+//
+//
+//    dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
+//    return !(found.0 == false && found.1 == false)
   }
   
+
   
   
   private func batchedIndexOf(range range: Range<Self.Index>, batchSize: Int, predicate: Generator.Element -> Bool, checkIfContinue: () -> Bool) -> Int? {
-    for startIndex in self.startIndex.stride(to: self.endIndex, by: batchSize) {
+    for startIndex in range.startIndex.stride(to: range.endIndex, by: batchSize) {
       let endIndex = min(startIndex + batchSize, self.count)
       for (index, item) in self[startIndex..<endIndex].enumerate() {
         if predicate(item) {
---
+++ Example/ConcurrentMapExample/CommonExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  CommonExtensions.swift
+//  ConcurrentMapExample
+//
+//  Created by Mark Aron Szulyovszky on 15/01/2016.
+//  Copyright © 2016 itchingpixels. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+  
+  subscript (safe key: Key) -> Value? {
+    if let foundValue = self[key] {
+      return foundValue
+    } else {
+      return nil
+    }
+  }
+  
+}
+
